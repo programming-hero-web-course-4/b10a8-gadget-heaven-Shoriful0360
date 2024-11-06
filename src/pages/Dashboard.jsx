912625c5142bbcd2d720516b443c940e0a8b7784
Shoriful <shorifulbba0360@gmail.com>
2024-11-06 14:57:12 +0600

    added purchase button functionality

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -7,23 +7,26 @@ import { getStoreDataList, getStoreWhistList, removeData, removeWishListstore } 
 import Product from '../Component/Product';
 import { useLoaderData, useLocation } from 'react-router-dom';
 import ProductDetails from './ProductDetails';
+import img from '../../src/assets/Group.png'
 
 const Dashboard = () => {
     const location = useLocation('/dashboard')
-    const allData=useLoaderData()
- 
+    const allData = useLoaderData()
+
     const [favorite, setFavorite] = useState([])
-    const [wishFavorite,setWishFavorite]=useState([])
-    const [active,setActive]=useState('null')
-  
-const [wishPrice,setWishPrice]=useState(0)
+    const [wishFavorite, setWishFavorite] = useState([])
+    const [active, setActive] = useState('null')
+
+    const [wishPrice, setWishPrice] = useState(0)
     const [price, setPrice] = useState(0)
+// for modal useState
+const [isOpenModal,setIsOpenModal]=useState(false)
     useEffect(() => {
         const getData = getStoreDataList()
-        const total=getData.reduce((acc,product)=>acc + (product.price || 0),0)
+        const total = getData.reduce((acc, product) => acc + (product.price || 0), 0)
         setPrice(total)
-        const getWishData=getStoreWhistList()
-        const totalWish=getWishData.reduce((add,product)=>add + (product.price || 0),0)
+        const getWishData = getStoreWhistList()
+        const totalWish = getWishData.reduce((add, product) => add + (product.price || 0), 0)
         setWishPrice(totalWish)
         setWishFavorite(getWishData)
 
@@ -31,41 +34,48 @@ const [wishPrice,setWishPrice]=useState(0)
 
 
 
-    }, [getStoreDataList,getStoreWhistList])
+    }, [getStoreDataList, getStoreWhistList])
 
-    const handleCartAndWishlistBtn=(name)=>{
-       setActive(name)
+    const handleCartAndWishlistBtn = (name) => {
+        setActive(name)
 
     }
 
-    
-    const handleDelet = (Id) => {      
-       removeData(Id)
-      
+
+    const handleDelet = (Id) => {
+        removeData(Id)
+
         const getData = getStoreDataList()
-        
+
         setFavorite(getData)
     }
 
-    const handleDeletWishList=(Id)=>{
+    const handleDeletWishList = (Id) => {
         removeWishListstore(Id)
-        const getWish=getStoreWhistList()
+        const getWish = getStoreWhistList()
         setWishFavorite(getWish)
     }
-    const handleSortPrice=(name)=>{
-        if(name==='addPrice'){
-            const sortPrice=[...favorite].sort((a,b)=>a.price - b.price)
+    const handleSortPrice = (name) => {
+        if (name === 'addPrice') {
+            const sortPrice = [...favorite].sort((a, b) => a.price - b.price)
             setFavorite(sortPrice)
-          
+
         }
     }
 
 
-    // add price functionality
+    // Modal functionality
 
-    const handleTotalAmount=(amount)=>{
-        console.log(amount)
+    const handleAllDeleteData=(name)=>{
+        // Id.showModal()
+        if(name==='delet'){
+            localStorage.removeItem('store')
+            setPrice(0)
+        }
+    
+        
     }
+
 
 
 
@@ -78,91 +88,132 @@ const [wishPrice,setWishPrice]=useState(0)
                         <p className="py-6">
                             Explore the latest gadgets that will take your experience to the next level. From smart devices to the coolest accessories, we have it all!
                         </p>
-                        <button onClick={()=>handleCartAndWishlistBtn('cart')} className={`btn text-begoni ${active==='cart'?'bg-warning':''} text-lg`}>Cart</button>
-                        <button  onClick={()=>handleCartAndWishlistBtn('wishList')} className={`btn ml-4 text-begoni ${active==='wishList'?'bg-warning':''} text-lg`}>WishList</button>
+                        <button onClick={() => handleCartAndWishlistBtn('cart')} className={`btn text-begoni ${active === 'cart' ? 'bg-warning' : ''} text-lg`}>Cart</button>
+                        <button onClick={() => handleCartAndWishlistBtn('wishList')} className={`btn ml-4 text-begoni ${active === 'wishList' ? 'bg-warning' : ''} text-lg`}>WishList</button>
                     </div>
                 </div>
             </div>
             <div className='flex justify-between mt-10'>
                 <div><h1 className='text-xl font-semibold'>Cart: {
-                    active==='null' && (
+                    active === 'null' && (
                         favorite.length
                     )
+                }
+                    {
+                        active === 'cart' && (
+                            favorite.length
+                        )
                     }
                     {
-                    active==='cart' && (
-                        favorite.length
-                    )
+                        active === 'wishList' && (
+                            wishFavorite.length
+                        )
                     }
-                    {
-                    active==='wishList' && (
-                        wishFavorite.length
-                    )
-                    }
-                    
-                    </h1></div>
+
+                </h1></div>
                 <div className='flex gap-6 items-center'>
-                    <h1 className='text-xl font-semibold'>Total cost: 
+                    <h1 className='text-xl font-semibold'>Total cost:
                         {/* for default  value */}
                         {
-                            active==='null' && (
+                            active === 'null' && (
                                 price.toFixed(2)
                             )
                         }
-                         {/* for cart price */}
-                         {
-                            active==='cart' && (
+                        {/* for cart price */}
+                        {
+                            active === 'cart' && (
                                 price.toFixed(2)
                             )
                         }
-                         {/* for wishList price   */}
-                         {
-                            active==='wishList' && (
+                        {/* for wishList price   */}
+                        {
+                            active === 'wishList' && (
                                 wishPrice.toFixed(2)
                             )
                         }
-                        
+
                         $ <small></small></h1>
-                    <button onClick={()=>handleSortPrice('addPrice')} className='btn'>Sort by price </button>
-                    <button className='btn'>Purchase</button>
+                    
+                    <button onClick={() => handleSortPrice('addPrice')} className='btn'>Sort by price </button>
+                    
+                    {/* Modal start */}
+                    {/* Open the modal using document.getElementById('ID').showModal() method */}
+                    <button className="btn" onClick={()=>document.getElementById('my_modal_1').showModal()}>Purchase</button>
+                    <dialog id="my_modal_1" className="modal">
+                            <div className="modal-box justify-center">
+                                <div className='flex justify-center'>
+                                <img src={img} alt="" />
+                                </div>
+                                <h3 className="font-bold text-lg">Confirm Purchase</h3>
+                                <p className="py-4">Are you sure you want to complete the purchase? This action will clear the cart</p>
+                                <h1 className='text-2xl font-bold'>Total Purchase: 
+                                       {/* for default  value */}
+                            {
+                                active === 'null' && (
+                                    price.toFixed(2)
+                                )
+                            }
+                            {/* for cart price */}
+                            {
+                                active === 'cart' && (
+                                    price.toFixed(2)
+                                )
+                            }
+                            {/* for wishList price   */}
+                            {
+                                active === 'wishList' && (
+                                    wishPrice.toFixed(2)
+                                )
+                            }$
+                                </h1>
+                                <div className="modal-action">
+                                    <form method="dialog">
+                                        {/* if there is a button in form, it will close the modal */}
+                                        <button onClick={()=>handleAllDeleteData('delet')} className="btn">Close</button>
+                                    </form>
+                                </div>
+                            </div>
+                        </dialog>
+                    {/* Modal end */}
+                    
                 </div>
             </div>
-            <h1  className='text-4xl font-bold mt-4'>Add Your Favorite Card</h1>
+            <h1 className='text-4xl font-bold mt-4'>Add Your Favorite Card</h1>
 
             {/* for dashboard default card */}
-           {
-            active ==='null' &&(
-                <div className='grid lg:grid-cols-3 md:grid-cols-2 gap-5'>
-                {
-                  favorite.map(product=><Product handleDelet={handleDelet}  key={product.produt_id} product={product}></Product>)
-                }
-              </div>
-            )
-           }
-
-           {/* for dashboard show cart */}
             {
-                active==='cart' &&(
+                active === 'null' && (
                     <div className='grid lg:grid-cols-3 md:grid-cols-2 gap-5'>
-                    {
-                      favorite.map(product=><Product handleDelet={handleDelet}  key={product.produt_id} product={product}></Product>)
-                    }
-                  </div>
+                        {
+                            favorite.map(product => <Product handleDelet={handleDelet} key={product.produt_id} product={product}></Product>)
+                        }
+                    </div>
+                )
+            }
+
+            {/* for dashboard show cart */}
+            {
+                active === 'cart' && (
+                    <div className='grid lg:grid-cols-3 md:grid-cols-2 gap-5'>
+                        {
+                            favorite.map(product => <Product handleDelet={handleDelet} key={product.produt_id} product={product}></Product>)
+                        }
+                    </div>
                 )
             }
             {/* for dashboard show wishList cart */}
 
             {
-                active==='wishList' && (
+                active === 'wishList' && (
                     <div className='grid lg:grid-cols-3 md:grid-cols-2 gap-5'>
-                    {
-                      wishFavorite.map(product=><Product handleDelet={handleDeletWishList}  key={product.produt_id} product={product}></Product>)
-                    }
-                  </div>
+                        {
+                            wishFavorite.map(product => <Product handleDelet={handleDeletWishList} key={product.produt_id} product={product}></Product>)
+                        }
+                    </div>
                 )
             }
-           
-          
+
+
 
         </div>
     );
